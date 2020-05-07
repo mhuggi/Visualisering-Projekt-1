7cd79f52ec478dfce451b25704b19038ae45c391
mhuggi <emil.e.salmi@gmail.com>
2020-05-07 12:47:41 +0300

    Added JSON files

---
+++ d3/builds/d3/lineJSON.js
@@ -0,0 +1,40 @@
+function drawChart() {
+
+
+    var width = 500;
+    var height = 300;
+
+    d3.json("lineData.json").get( function(error, dataArray) {
+        console.log(dataArray);
+
+        var xs = [];
+        var ys = [];
+
+        for (i=0; i<dataArray.length; i++) {
+            xs.push(dataArray[i].x);
+            ys.push(dataArray[i].y);
+
+        }
+        console.log(xs);
+        console.log(ys);
+
+    var canvas = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+    var path = d3.line()
+    .x(function(data) {return data.x * 5})
+    .y(function(data) {return data.y * 5});
+
+    //Rita en linje
+    canvas.append("path")
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("d",path(dataArray));
+
+
+});
+
+
+}
