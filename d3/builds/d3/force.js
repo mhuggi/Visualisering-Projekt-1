9a7f21491ec0d29797b57017b0867a468712fe83
mhuggi <emil.e.salmi@gmail.com>
2020-05-22 12:35:33 +0300

    Update force.js

--- d3/builds/d3/force.js
+++ d3/builds/d3/force.js
@@ -1,19 +1,32 @@
 function drawChart() {
     d3.json("nodes.json", function(data) {
         var width = 400, height = 400;
+        
 
         var canvas = d3.select("body").append("svg")
             .attr("width", width).attr("height", height);
 
-
-        var node = canvas.append("g").selectAll("nodes").data(data)
+        console.log(data.nodes);
+        var node = canvas.append("g").selectAll("nodes").data(data.nodes)
         .enter()
             .append("ellipse")
             .attr("cx", 100)
             .attr("cy", 100)
-            .attr("r", 20)
-            .attr("stroke", "green")
+            .attr("rx", 20)
+            .attr("ry", 20)
+            .attr("stroke", "black")
             .attr("fill", "lightgray");
 
+
+            var simulation = d3.forceSimulation()
+                .force("charge", d3.forceManyBody().strength(-20))
+                .force("center", d3.forceCenter(width / 2, height /2));
+
+            simulation.nodes(data.nodes).on("tick", tickHandler);
+
+            function tickHandler() {
+                node.attr("cx", function(d) {return d.x})
+                node.attr("cy", function(d) {return d.y});
+            }
     });
 };
